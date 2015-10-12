773d6441d4523bc0c06f5a827e9b4c3ce158e6f6
Roxanne <cwillenbockel@aol.com>
2015-10-12 22:29:39 +0200

    First commit

---
+++ SandrahRtF/Areas/RTFUnpack.bat
@@ -0,0 +1,82 @@
+@echo off
+cd override
+tisunpack -f RTF010.tiz
+tisunpack -f RTF002.tiz
+tisunpack -f RTF003.tiz
+tisunpack -f RTF004.tiz
+tisunpack -f RTF005.tiz
+tisunpack -f RTF006.tiz
+tisunpack -f RTF021.tiz
+tisunpack -f RTF023.tiz
+tisunpack -f RTF030.tiz
+tisunpack -f RTF034.tiz
+tisunpack -f RTF040.tiz
+tisunpack -f RTF045.tiz
+tisunpack -f RTF048.tiz
+tisunpack -f RTF049.tiz
+tisunpack -f RTF050.tiz
+tisunpack -f RTF060.tiz
+tisunpack -f RTF070.tiz
+tisunpack -f RTF071.tiz
+tisunpack -f RTF072.tiz
+tisunpack -f RTF073.tiz
+tisunpack -f RTF074.tiz
+tisunpack -f RTF077.tiz
+tisunpack -f RTF082.tiz
+tisunpack -f RTF090.tiz
+tisunpack -f RTF111.tiz
+tisunpack -f RTF120.tiz
+tisunpack -f RTF121.tiz
+tisunpack -f RTF123.tiz
+tisunpack -f RTF141.tiz
+tisunpack -f RTF142.tiz
+tisunpack -f RTF143.tiz
+tisunpack -f RTF130.tiz
+tisunpack -f RTF150.tiz
+tisunpack -f RTF151.tiz
+tisunpack -f RTF161.tiz
+tisunpack -f RTF162.tiz
+tisunpack -f RTF401.tiz
+tisunpack -f CVRTF8.tiz
+tisunpack -f CVRTF9.tiz
+del RTF002.tiz
+del RTF003.tiz
+del RTF004.tiz
+del RTF005.tiz
+del RTF006.tiz
+del RTF010.tiz
+del RTF021.tiz
+del RTF023.tiz
+del RTF030.tiz
+del RTF034.tiz
+del RTF040.tiz
+del RTF045.tiz
+del RTF048.tiz
+del RTF049.tiz
+del RTF050.tiz
+del RTF060.tiz
+del RTF070.tiz
+del RTF071.tiz
+del RTF072.tiz
+del RTF073.tiz
+del RTF074.tiz
+del RTF077.tiz
+del RTF082.tiz
+del RTF090.tiz
+del RTF111.tiz
+del RTF121.tiz
+del RTF120.tiz
+del RTF123.tiz
+del RTF130.tiz
+del RTF141.tiz
+del RTF142.tiz
+del RTF143.tiz
+del RTF150.tiz
+del RTF151.tiz
+del RTF161.tiz
+del RTF162.tiz
+del RTF401.tiz
+del CVRTF8.tiz
+del CVRTF9.tiz
+del tisunpack.exe
+cd ..
